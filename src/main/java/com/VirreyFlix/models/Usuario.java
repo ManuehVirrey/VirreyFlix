6354245873115486267f90077df24443bc0f1c38
Manueh <jro634222@iesvirreymorcillo.es>
2025-02-04 17:16:08 +0100

    Creacion de CRUD de Usuarios

--- src/main/java/com/VirreyFlix/models/Usuario.java
+++ src/main/java/com/VirreyFlix/models/Usuario.java
@@ -1,9 +1,17 @@
 package com.VirreyFlix.models;
 
+import com.VirreyFlix.HibernateUtil;
 import jakarta.persistence.*;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.Scanner;
 
 @Entity
 public class Usuario {
+
+    public static Scanner sc = new Scanner(System.in);
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -47,5 +55,81 @@ public class Usuario {
         this.email = email;
     }
 
+    public void crearUsuario() {
 
+        String nNombre, nEmail;
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        System.out.println("Ingresa el nombre del nuevo Usuario: ");
+        nNombre = sc.next();
+
+        System.out.println("Ingresa el email del nuevo Usuario: ");
+        nEmail = sc.next();
+
+        Usuario u = new Usuario(nNombre, nEmail);
+        session.persist(u);
+
+        tx.commit();
+        session.close();
+    }
+
+    public void mostrarUsuarios() {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        List<Usuario> listaUsuarios = session.createQuery("from Usuario", Usuario.class).list();
+
+        for (Usuario u : listaUsuarios) {
+            System.out.println("Usuarios: " + u.nombre + " Mail: " + u.email);
+        }
+        session.close();
+    }
+
+    public void actualizarUsuario() {
+        String nNombre, nEmail;
+        int id;
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        System.out.println("Ingresa el id del usuario a modificar: ");
+        id = sc.nextInt();
+        for (Usuario u : session.createQuery("from Usuario", Usuario.class).list()) {
+            if (u.getId() == id) {
+                System.out.println("Ingresa el nuevo nombre: ");
+                nNombre = sc.next();
+
+                System.out.println("Ingresa el nuevo email: ");
+                nEmail = sc.next();
+
+                u.setNombre(nNombre);
+                u.setEmail(nEmail);
+
+                session.update(u);
+                tx.commit();
+                session.close();
+                System.out.println("Usuario modificado exitosamente");
+                return;
+            } else {
+                System.out.println("Id no encontrado");
+                session.close();
+                return;
+            }
+        }
+    }
+
+    public void eliminarUsuario() {
+        int id;
+        System.out.println("Ingresa el id del usuario a eliminar: ");
+        id = sc.nextInt();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        Usuario u = session.get(Usuario.class, id);
+        session.remove(u);
+        tx.commit();
+        session.close();
+        System.out.println("Usuario eliminado exitosamente");
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import com.VirreyFlix.models.Usuario;
+
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        menuPrincipal();
+    }
+
+    public static void menuPrincipal() {
         int opcion;
         do {
             System.out.println("Menu:");
@@ -17,9 +23,40 @@ public class Main {
             System.out.println("0. Salir");
             opcion = sc.nextInt();
 
-        }
-        while (opcion != 0);
+            switch (opcion) {
+                case 1 ->usuarioCRUD();
+//                case 2 ->perfilCRUD();
+//                case 3 ->serieCRUD();
+//                case 4 ->episodioCRUD();
+//                case 5 ->historialCRUD();
+//                case 6 ->consultas();
+                case 0 -> System.out.println("Saliendo de la aplicacion...");
 
-        sc.close();
+                default -> System.out.println("Opcion no valida");
+            }
+        }while (opcion != 0);
+    }
+
+    public static void usuarioCRUD() {
+        Usuario u = new Usuario();
+        int opcionsur;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Crear Usuario");
+            System.out.println("2. Mostrar Usuarios");
+            System.out.println("3. Actualizar Usuario");
+            System.out.println("4. Eliminar Usuario");
+            System.out.println("0. Volver al menu principal");
+            opcionsur = sc.nextInt();
+
+            switch (opcionsur) {
+                case 1 -> u.crearUsuario();
+                case 2 -> u.mostrarUsuarios();
+                case 3 -> u.actualizarUsuario();
+                case 4 -> u.eliminarUsuario();
+                case 0 -> menuPrincipal();
+                default -> System.out.println("Opcion no valida");
+            }
+        } while (opcionsur != 0);
     }
 }
